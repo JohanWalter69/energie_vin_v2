ec62d15caaab967a26854ca68af98b3566f6a62e
Johan Walter <walter.johan25@gmail.com>
2023-02-11 20:20:09 +0100

    search bar done

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,8 @@
 class ProductsController < ApplicationController
   def index
     @reviews = Review.all
-    if params[:max_price].present?
-      @products = Product.where(price: params[:max_price])
+    if params[:min_price].present? && params[:max_price].present?
+      @products = Product.where(price: ..params[:max_price]).where(price: params[:min_price]..)
     else
       @products = Product.all
     end
